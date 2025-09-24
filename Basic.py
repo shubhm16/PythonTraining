ae8ecf366e56121bb210ba8d6a7b1449e12d074f
shubhm16 <60982870+shubhm16@users.noreply.github.com>
2025-09-24 09:45:01 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,48 @@
+''' #print("This is my first program");
+
+# TO print multiple line
+""" use to comment multiple line"""
+
+#print("""This is my first program
+#and this is new line """);
+
+#print("This is next method to print \n multiple line ");
+
+
+
+thisdict = {
+  "brand": ("Ford","TVS","Petrol"),
+  "model": "Mustang",
+  "model":"car",
+  "year": 1964,
+  "year":2002
+}
+#thisdict["model"]="Sedan"
+#xIndex = list(thisdict.keys("brand")).index(1)
+print(thisdict)
+#print(str(xIndex))'''
+
+#IF condition
+'''a=20
+if a<=20:
+   print("yes")
+else:
+  print("no")'''
+
+
+#ifelif
+
+'''i=20
+if i<20:
+  print("Less than")
+elif i>20:
+  print("greater than")
+else:
+  print("equal to")'''
+
+Student = ["Shubham","Rakesh"]
+if "Ram" in Student:
+  print("hai")
+else:
+  print("nahi hai")
+
